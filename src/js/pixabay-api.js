cd9bed9425b29d4f03782baca8fc03b2bc4c1423
Roman Drok <rookglli@gmail.com>
2026-06-27 21:18:45 +0400

    request on Pixabay

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '41651325-d56a057956804a71a345fad13';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
